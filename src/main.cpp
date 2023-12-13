ca833281821349b0a0bf45e8b72e6f90ea4dfc42
Tyler <tylergross28@gmail.com>
2023-12-13 16:48:29 -0500

    more things

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include "../deps/include/png.h"
+#include "../deps/include/libpng16/png.h"
 
 int main() {
+  png_image image{};
+  png_image_begin_read_from_file(&image, "stuff");
+
   std::cout << "hiiiiiii..... bud" << std::endl;
 }
